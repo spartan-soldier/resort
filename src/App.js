77c0a2226f5821327e5126dbbe1648da7380c858
Abhay Shekhawat <shekhawat.abhay94@gmail.com>
2020-09-09 22:17:03 +0530

    a basic structure setup with routing in a very simple manner

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/home.component';
+import Rooms from './pages/rooms.component';
+import SingleRoom from './pages/single-room.component';
+import Error from './pages/error.component';
+
+import {Route,Link,Switch} from 'react-router-dom';
+
+import Navbar from './components/navbar.component';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Switch>
+        <Route exact path ='/' component = {Home}/>
+        <Route exact path ='/rooms/' component = {Rooms}/>
+        <Route exact path ='/rooms/:slug' component = {SingleRoom}/>
+        <Route component = {Error}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/home.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+           Hello from Home Page 
+        </div>
+    )
+}
+
+
+export default Home;
